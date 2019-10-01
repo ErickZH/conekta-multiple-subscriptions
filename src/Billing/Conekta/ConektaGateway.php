e0fcf07b58579c22cfe43b348eb51996bb55dc81
ErickZH <zaraterick@outlook.com>
2019-10-01 11:49:15 -0500

    Install conekta

--- src/Billing/Conekta/ConektaGateway.php
+++ src/Billing/Conekta/ConektaGateway.php
@@ -4,5 +4,30 @@ namespace JJE\Billing\Conekta;
 
 class ConektaGateway
 {
-    //
+    /** @var string */
+    private $apiSecret;
+
+    /** @var string */
+    private $apiVersion;
+
+    /** @var string */
+    private $locale;
+
+    /**
+     * ConektaGateway constructor.
+     *
+     * @param  string  $apiSecret
+     * @param  string  $apiVersion
+     * @param  string  $locale
+     */
+    public function __construct(string $apiSecret, string $apiVersion, string $locale = 'es')
+    {
+        $this->apiSecret = $apiSecret;
+        $this->apiVersion = $apiVersion;
+        $this->locale = $locale;
+
+        \Conekta\Conekta::setApiKey($apiSecret);
+        \Conekta\Conekta::setApiVersion($apiVersion);
+        \Conekta\Conekta::setLocale($locale);
+    }
 }
